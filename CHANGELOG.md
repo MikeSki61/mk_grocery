6ed915326038c24bbde5e04bfd83a6619eb2060e
Mike Kwiatkowsky <mikeski885@gmail.com>
2025-11-19 11:03:11 -0500

    complete day 3 exercises, updated chamgelog for version0.2.0 with new exercises

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,8 @@
 - The first version, including all code up to day 3 of the 14 day coder course.
 - This includes learning lists and tuples, dictionaries and sets.
 -Implemented changgelog and version control insturctions.
+
+## [0.2.0]  - 2025-11-19
+## -exercises for tuples, dictionaries, sets and slicing lists.
+
+-
--- my_script.py
+++ my_script.py
@@ -186,76 +186,76 @@ print(my_tuple + (30, 40))
 # set_x = {'cat', 'dog', 'fish'}
 # set_y = {'dog', 'bird'}
 # print(set_x)
-# print(set_y)
-# print(set_x.difference(set_y))
+# # print(set_y)
+# # print(set_x.difference(set_y))
 
-# ============================================
-# Day 3 Required Assignments
+# # ============================================
+# # Day 3 Required Assignments
 
-# Exercise 1
-# 1
-bananas_tuple = ('bananas', 0.73, 8)
-cherries_tuple = ('cherries', 1.16, 4)
-apples_tuple = ('apples', 1.27, 5)
-# 2
-grocery_list = []
-print(grocery_list)
-grocery_list = [bananas_tuple] + [cherries_tuple] + [apples_tuple]
-print(grocery_list)
-print(grocery_list[0])
-print(grocery_list[1])
-print(grocery_list[-1])
+# # Exercise 1
+# # 1
+# bananas_tuple = ('bananas', 0.73, 8)
+# cherries_tuple = ('cherries', 1.16, 4)
+# apples_tuple = ('apples', 1.27, 5)
+# # 2
+# grocery_list = []
+# print(grocery_list)
+# grocery_list = [bananas_tuple] + [cherries_tuple] + [apples_tuple]
+# print(grocery_list)
+# print(grocery_list[0])
+# print(grocery_list[1])
+# print(grocery_list[-1])
 
-#3
-bananas_total_cost = grocery_list[0][1] * grocery_list[0][2]
-cherries_total_cost = grocery_list[1][1] * grocery_list[1][2]
-apples_total_cost = grocery_list[2][1] * grocery_list[2][2]
-print(f'Total cost of {grocery_list[0][0]}: ${bananas_total_cost}')
-print(f'Total cost of {grocery_list[1][0]}: ${cherries_total_cost}')
-print(f'Total cost of {grocery_list[2][0]}: ${apples_total_cost}')
+# #3
+# bananas_total_cost = grocery_list[0][1] * grocery_list[0][2]
+# cherries_total_cost = grocery_list[1][1] * grocery_list[1][2]
+# apples_total_cost = grocery_list[2][1] * grocery_list[2][2]
+# print(f'Total cost of {grocery_list[0][0]}: ${bananas_total_cost}')
+# print(f'Total cost of {grocery_list[1][0]}: ${cherries_total_cost}')
+# print(f'Total cost of {grocery_list[2][0]}: ${apples_total_cost}')
 
-# Exercise 2
-# 1
-bananas_dict = {'name': 'bananas', 'price': 0.73, 'quantity': 8, 'total_cost': 5.84}
-cherries_dict = {'name': 'cherries', 'price': 1.16, 'quantity': 4, 'total_cost': 4.64}
-apples_dict = {'name': 'apples', 'price': 1.27, 'quantity': 5, 'total_cost': 6.35}
-print(f'Total cost of {bananas_dict['name']}: ${bananas_dict['total_cost']}')
-print(f'Total cost of {cherries_dict['name']}: ${cherries_dict['total_cost']}')
-print(f'Total cost of {apples_dict['name']}: ${round(apples_dict['total_cost'],2)}')
+# # Exercise 2
+# # 1
+# bananas_dict = {'name': 'bananas', 'price': 0.73, 'quantity': 8, 'total_cost': 5.84}
+# cherries_dict = {'name': 'cherries', 'price': 1.16, 'quantity': 4, 'total_cost': 4.64}
+# apples_dict = {'name': 'apples', 'price': 1.27, 'quantity': 5, 'total_cost': 6.35}
+# print(f'Total cost of {bananas_dict['name']}: ${bananas_dict['total_cost']}')
+# print(f'Total cost of {cherries_dict['name']}: ${cherries_dict['total_cost']}')
+# print(f'Total cost of {apples_dict['name']}: ${round(apples_dict['total_cost'],2)}')
 
-# Exercise 3
-# 1
-num_list = [16, 47, 1, 3, 5, 9, 15, 2]
-print(num_list)
-print(num_list[1:])
-print(num_list[:3])
-print(num_list[-3])
+# # Exercise 3
+# # 1
+# num_list = [16, 47, 1, 3, 5, 9, 15, 2]
+# print(num_list)
+# print(num_list[1:])
+# print(num_list[:3])
+# print(num_list[-3])
 
-# 2
-num_list.sort()
-print(num_list)
+# # 2
+# num_list.sort()
+# print(num_list)
 
-#3
-print(len(num_list))
+# #3
+# print(len(num_list))
 
-# Exercise 4
-# 1
-dairy_products = {'milk', 'butter', 'cream', 'yogurt', 'cheese'}
-print(dairy_products)
-desserts = {'jello', 'chocolate', 'candy', 'cookies', 'muffins'}
-print(desserts)
+# # Exercise 4
+# # 1
+# dairy_products = {'milk', 'butter', 'cream', 'yogurt', 'cheese'}
+# print(dairy_products)
+# desserts = {'jello', 'chocolate', 'candy', 'cookies', 'muffins'}
+# print(desserts)
 
-# 2
-dairy_products.add('ice creame')
-print(dairy_products)
-desserts.add('ice creame')
-print(desserts)
+# # 2
+# dairy_products.add('ice creame')
+# print(dairy_products)
+# desserts.add('ice creame')
+# print(desserts)
 
-# 3
-dairy_products.remove('cream')
-print(dairy_products)
-desserts.discard('jello')
-print(desserts)
+# # 3
+# dairy_products.remove('cream')
+# print(dairy_products)
+# desserts.discard('jello')
+# print(desserts)
 
-# 4
-print(dairy_products.intersection(desserts))
+# # 4
+# print(dairy_products.intersection(desserts))
